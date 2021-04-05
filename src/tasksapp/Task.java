48a1ef62dccf336e21b21f59658e0bb5a253409f
waltermolina <waltermolina@msn.com>
2021-04-05 16:51:27 -0300

    agregué comment

--- src/tasksapp/Task.java
+++ src/tasksapp/Task.java
@@ -3,6 +3,7 @@
  * @author: waltermolina
  * @version: 20210405.1
  * @see <a href = "http://github.com/" /> Repositorio en Github </a>
+ * asdasdasdasdasd asdas dasd asd
  */
 package tasksapp; //carpeta 
 
